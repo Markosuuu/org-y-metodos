baae41131dbe5d281c97d176ebc93fdd59639ecf
Marcos Diaz <101750266+Markosuuu@users.noreply.github.com>
2022-10-28 23:35:33 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -61,13 +61,13 @@ function faltaFeriado(sueldo) {
 }
 
 // Función para calcular las horas extras en 50% y 100%
-function hsExtras(sueldo){
+function hsExtras(sueldo) {
   let h50 = document.querySelector(".extrasAl50").value;
   let h100 = document.querySelector(".extrasAl100").value;
 
-  let valorHs = sueldo / 176
+  let valorHs = sueldo / 176;
 
-  let horasExtras = ((h50 * 1.5) * valorHs) + ((h100 * 2) * valorHs)
+  let horasExtras = h50 * 1.5 * valorHs + h100 * 2 * valorHs;
 
   if (horasExtras > 0) {
     return horasExtras;
@@ -77,15 +77,15 @@ function hsExtras(sueldo){
 }
 
 //Funcion para calcular cuantos dias le corresponde de vacaciones
-function diasVacaciones () {
-  let fecha = (new Date(document.querySelector(".fecha").value))
+function diasVacaciones() {
+  let fecha = new Date(document.querySelector(".fecha").value);
 
   //para tomar la fecha actual
-  let hoy = new Date()
+  let hoy = new Date();
 
   // `getMonth()` devuelve el mes (de 0 a 11)
   let mesA = hoy.getMonth() + 1;
-  
+
   // `getFullYear()` devuelve el año completo
   let anioA = hoy.getFullYear();
 
@@ -94,45 +94,44 @@ function diasVacaciones () {
 
   let anioI = fecha.getFullYear();
 
-  let anioAntiguedad = anioA - anioI
+  let anioAntiguedad = anioA - anioI;
 
   //diferencia entre el mes actual y el mes de ingreso
-  let mesAntiguedad = mesA - mesI
+  let mesAntiguedad = mesA - mesI;
 
   //Calcula lo que le corresponde de vacaciones
   let diasVacaciones = 0;
 
   // Si tiene mas de 20 años de antiguedad le corresponde 35 dias
-	if (anioAntiguedad >= 20) {
-		diasVacaciones =+ 35
-	}
+  if (anioAntiguedad >= 20) {
+    diasVacaciones = +35;
+  }
   // Si tiene entre 20 y 10 años de antiguedad le corresponde 28
-	else if (anioAntiguedad >= 10 && anioAntiguedad < 20){
-		diasVacaciones =+ 28
-	}
+  else if (anioAntiguedad >= 10 && anioAntiguedad < 20) {
+    diasVacaciones = +28;
+  }
   // Si tiene entre 5 y 10 le corresponde 21
-	else if (anioAntiguedad >= 5 && anioAntiguedad < 10){
-		diasVacaciones =+ 21
-	}
+  else if (anioAntiguedad >= 5 && anioAntiguedad < 10) {
+    diasVacaciones = +21;
+  }
   // Si tiene menos de 5 le puede corresponder 14 o la diferencia entre el mes actual y el mes que ingreso
-	else if (anioAntiguedad < 5){
-		if (anioAntiguedad == 0 && mesAntiguedad < 6){
-			diasVacaciones =+ mesAntiguedad
-		}
-		else {
-			diasVacaciones =+ 14
-		}
-	}
+  else if (anioAntiguedad < 5) {
+    if (anioAntiguedad == 0 && mesAntiguedad < 6) {
+      diasVacaciones = +mesAntiguedad;
+    } else {
+      diasVacaciones = +14;
+    }
+  }
 
-  return diasVacaciones
+  return diasVacaciones;
 }
 
 // Funcion para calcular cuanto le corresponde de paga por vacaciones
 
-function pagoVacaciones (sueldo) {
-  let pagoVacas =+ sueldoBasico(sueldo) / 22 * 1.10 * 1.25 * diasVacaciones()
-  
-  return pagoVacas
+function pagoVacaciones(sueldo) {
+  let pagoVacas = (+sueldoBasico(sueldo) / 22) * 1.1 * 1.25 * diasVacaciones();
+
+  return pagoVacas;
 }
 
 // Función para calcular el sueldo bruto
@@ -147,19 +146,19 @@ function sueldoBruto(sueldo) {
   );
 }
 
-const aguinaldo  = (sueldo) =>{
-  let aguinaldo = sueldoBruto(sueldo)/2
+const aguinaldo = (sueldo) => {
+  let aguinaldo = sueldoBruto(sueldo) / 2;
 
-  return Number (aguinaldo)
-}
+  return Number(aguinaldo);
+};
 
 //Función para calcular el sueldo neto
-function sueldoNeto (sueldo) {
-  let obraSocialYLey = sueldoBruto(sueldo) * (3/100);
+function sueldoNeto(sueldo) {
+  let obraSocialYLey = sueldoBruto(sueldo) * (3 / 100);
 
-  let jubilacionLey = sueldoBruto(sueldo) * (11/100);
+  let jubilacionLey = sueldoBruto(sueldo) * (11 / 100);
 
-  return Number(sueldoBruto(sueldo) - obraSocialYLey * 2 - jubilacionLey); 
+  return Number(sueldoBruto(sueldo) - obraSocialYLey * 2 - jubilacionLey);
 }
 
 // Función principal que muestra toda la info al apretar el botón
@@ -179,22 +178,22 @@ const liquidacion = () => {
   let legajo = document.querySelector(".legajo").value;
 
   // Ingreso del trabajador
-  let ingreso = (new Date(document.querySelector(".fecha").value))
+  let ingreso = new Date(document.querySelector(".fecha").value);
 
   // Retenciones
-  let jubilacionLey = sueldoBruto(sueldo) * (11/100);
-  let obraSocialYLey = sueldoBruto(sueldo) * (3/100);
+  let jubilacionLey = sueldoBruto(sueldo) * (11 / 100);
+  let obraSocialYLey = sueldoBruto(sueldo) * (3 / 100);
 
   // Tomando los valores importantes de la fecha
-  let diai = ingreso.getDate()
-  let mesi = ingreso.getMonth() + 1
-  let anioi = ingreso.getFullYear()
+  let diai = ingreso.getDate();
+  let mesi = ingreso.getMonth() + 1;
+  let anioi = ingreso.getFullYear();
 
   // Concatenando la fecha de ingreso
-  let fe = `${diai+1}/${mesi}/${anioi}`
+  let fe = `${diai + 1}/${mesi}/${anioi}`;
 
-  //toma el dia de hoy 
-  let hoy = new Date()
+  //toma el dia de hoy
+  let hoy = new Date();
 
   let dia = hoy.getDate();
   // `getMonth()` devuelve el mes (de 0 a 11)
@@ -202,15 +201,15 @@ const liquidacion = () => {
   // `getFullYear()` devuelve el año completo
   let anio = hoy.getFullYear();
 
-  let fecha= `${dia}/${mes}/${anio}`
-  
+  let fecha = `${dia}/${mes}/${anio}`;
+
   // Tomando las campos de la tabla
   const fechaTabla = document.querySelector(".fechaTabla");
   const nombreTabla = document.querySelector(".nombreTabla");
   const apellidoTabla = document.querySelector(".apellidoTabla");
   const legajoTabla = document.querySelector(".legajoTabla");
   const inicioTabla = document.querySelector(".inicioTabla");
-  
+
   // Ingresando valores en la tabla
   fechaTabla.textContent = fecha;
   nombreTabla.textContent = nombre;
@@ -239,36 +238,44 @@ const liquidacion = () => {
   leyTabla.textContent = `$${obraSocialYLey.toFixed(2)}`;
 
   // Valores de los radioButtons
-  const radioAguinaldo = document.querySelector('input[name="aguinaldo"]:checked').value;
+  const radioAguinaldo = document.querySelector(
+    'input[name="aguinaldo"]:checked'
+  ).value;
   const aguinaldoRow = document.querySelector(".aguinaldoRow");
 
-  if (radioAguinaldo === 'Si') {
-    aguinaldoRow.classList.remove("d-none")
-  } else if (radioAguinaldo === 'No') {
-    aguinaldoRow.classList.add("d-none")
+  if (radioAguinaldo === "Si") {
+    aguinaldoRow.classList.remove("d-none");
+  } else if (radioAguinaldo === "No") {
+    aguinaldoRow.classList.add("d-none");
   }
 
-  const radioVacaciones = document.querySelector('input[name="vacaciones"]:checked').value;
+  const radioVacaciones = document.querySelector(
+    'input[name="vacaciones"]:checked'
+  ).value;
   const vacacionesRow = document.querySelector(".vacacionesRow");
   const vacacionesRow2 = document.querySelector(".vacacionesRow2");
 
-  if (radioVacaciones === 'Si') {
-    vacacionesRow.classList.remove("d-none")
-    vacacionesRow2.classList.remove("d-none")
-  } else if (radioVacaciones === 'No') {
-    vacacionesRow.classList.add("d-none")
-    vacacionesRow2.classList.add("d-none")
+  if (radioVacaciones === "Si") {
+    vacacionesRow.classList.remove("d-none");
+    vacacionesRow2.classList.remove("d-none");
+  } else if (radioVacaciones === "No") {
+    vacacionesRow.classList.add("d-none");
+    vacacionesRow2.classList.add("d-none");
   }
 
   // Aguinaldo y vacaciones
   const aguinaldoTabla = document.querySelector(".aguinaldoTabla");
   const vacacionesTabla = document.querySelector(".vacacionesTabla");
-  const remuneracionVacacionesTabla = document.querySelector(".remuneracionVacacionesTabla");
+  const remuneracionVacacionesTabla = document.querySelector(
+    ".remuneracionVacacionesTabla"
+  );
 
   aguinaldoTabla.textContent = `$${aguinaldo(sueldo).toFixed(2)}`;
   vacacionesTabla.textContent = `${diasVacaciones()}`;
-  remuneracionVacacionesTabla.textContent = `$${pagoVacaciones(sueldo).toFixed(2)}`;
-  
+  remuneracionVacacionesTabla.textContent = `$${pagoVacaciones(sueldo).toFixed(
+    2
+  )}`;
+
   // Sueldo bruto y neto
   const sueldoBrutoTabla = document.querySelector(".sueldoBrutoTabla");
   const sueldoNetoTabla = document.querySelector(".sueldoNetoTabla");
@@ -276,3 +283,39 @@ const liquidacion = () => {
   sueldoBrutoTabla.textContent = `$${sueldoBruto(sueldo).toFixed(2)}`;
   sueldoNetoTabla.textContent = `$${sueldoNeto(sueldo).toFixed(2)}`;
 };
+
+function deshabilitar(e) {
+  e.preventDefault();
+}
+
+function datos() {
+  // Sueldo básico
+  let sueldo = document.querySelector(".sueldo").value;
+
+  // Nombre
+  let nombre = document.querySelector(".nombre").value;
+
+  // Apellido
+  let apellido = document.querySelector(".apellido").value;
+
+  // Legajo
+  let legajo = document.querySelector(".legajo").value;
+
+  // Ingreso del trabajador
+  let ingreso = new Date(document.querySelector(".fecha").value);
+
+  
+  if (
+    nombre === "" ||
+    apellido === "" ||
+    sueldo === "" ||
+    legajo === "" ||
+    isNaN(ingreso.getDate())
+    ) {
+    alert("Faltó completar campos");
+    cont.classList.add("d-none");
+  } else {
+    console.log(ingreso.getDate())
+    liquidacion();
+  }
+}
